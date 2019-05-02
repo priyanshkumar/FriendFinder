6d022d5eba13cebabd2c66f66a3dd872db94d0e8
Priyansh <pradadiya667@gmail.com>
2019-05-02 13:43:19 -0400

    updated server.js with middelware for post request

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var express = require("express");
 var PORT = process.env.PORT || 3000;
 var app = express;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(PORT, function() {
   console.log(`Server running on Port ${PORT}`);
 });
