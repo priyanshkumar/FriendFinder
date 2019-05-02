1e04cad7ef9a61ead212c87a45e9e3d2e4aa0156
Priyansh <pradadiya667@gmail.com>
2019-05-02 14:02:09 -0400

    exported app from htmlRoutes to server.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,13 @@
 var path = require("path");
-app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function(req, res) {
-  res.sendFile("/home.html");
-});
+module.exports = function(app) {
+  app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/survey", function(req, res) {
-  res.sendFile("/survey.html");
-});
+  app.get("/", function(req, res) {
+    res.sendFile("/home.html");
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile("/survey.html");
+  });
+};
