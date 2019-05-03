4a6ff8c2074c837adb38c8fe891f4f8adeee81b8
Priyansh <pradadiya667@gmail.com>
2019-05-03 16:53:09 -0400

    Updated apiRoutes with logic to check for the match

--- app/data/friend.js
+++ app/data/friend.js
@@ -9,7 +9,7 @@ var friend = [
     name: "Pr",
     photo:
       "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    scores: [3, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,9 +8,24 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res) {
     var friend = req.body;
+    var tmpScore = 0;
+    var match = 51;
+    var index;
+
+    for (var i = 0; i < friends.length; i++) {
+      for (var j = 0; j < friends[i].scores.length; j++) {
+        tmpScore += Math.abs(friend.scores[j] - friends[i].scores[j]);
+      }
+
+      if (match > tmpScore) {
+        match = tmpScore;
+        index = i;
+      }
+      tmpScore = 0;
+    }
 
     friends.push(friend);
 
-    res.send(friends);
+    res.send(friends[index].name);
   });
 };
