938d783e70a926080add533f33f0e2fd23d09414
Priyansh <pradadiya667@gmail.com>
2019-05-03 14:08:18 -0400

    made changes so that i could add friends to my data base with post request

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,12 +3,14 @@ var friends = require("../data/friend");
 
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
-    res.json(friend);
+    res.json(friends);
   });
 
   app.post("/api/friends", function(req, res) {
     var friend = req.body;
 
     friends.push(friend);
+
+    res.send(friends);
   });
 };
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require("express");
+var express = require("express");
 
-const PORT = process.env.PORT || 3000;
-const app = express();
-
-require("./app/routing/htmlRoutes")(app);
-require("./app/routing/apiRoutes")(app);
+var PORT = process.env.PORT || 3000;
+var app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 
 app.listen(PORT, function() {
   console.log(`Server running on Port ${PORT}`);
